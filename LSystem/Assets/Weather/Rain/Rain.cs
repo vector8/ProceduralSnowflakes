fabccbfcfdbb475c3a1e801895b2ebcdeb42defb
Chris <Chris@localhost>
2019-04-09 16:18:36 -0400

    Added functionality to write snowflake to texture, and then to png file. Added weather system to test snowflakes.

---
+++ LSystem/Assets/Weather/Rain/Rain.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rain : MonoBehaviour {
+
+    public float m_RadiusOfCloud;
+    [Range(0.0f, 1.0f)]
+    public float m_RainIntensity;
+    private float m_RainIntensityCheck;
+    ParticleSystem m_Rain;
+    float m_EmissionRate;
+    ParticleSystem.EmissionModule m_Emission;
+    public int m_MaxParticles=20000;
+
+    public void SetIntensity(float a_RainIntensity)
+    {
+        m_Rain.maxParticles = (int)(m_MaxParticles * a_RainIntensity);
+        m_Rain.emissionRate = (int)(m_MaxParticles * a_RainIntensity);
+    }
+    private void Awake()
+    {
+       //m_Emission = GetComponent<ParticleSystem>().emission;
+       //m_Emission.rateOverTime = new ParticleSystem.MinMaxCurve(1.0f, m_RainPattern);
+    }
+    // Use this for initialization
+    void Start () {
+        m_RainIntensityCheck = m_RainIntensity;
+        m_Rain = gameObject.GetComponent<ParticleSystem>();
+        m_Rain.emissionRate = (int)(m_MaxParticles * m_RainIntensity);
+        m_EmissionRate = m_Rain.emissionRate;
+        m_Rain.maxParticles = (int)(m_MaxParticles * m_RainIntensity);
+        m_Emission = GetComponent<ParticleSystem>().emission;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (m_RainIntensity != m_RainIntensityCheck)
+        {
+            SetIntensity(m_RainIntensity);
+            m_RainIntensityCheck = m_RainIntensity;
+        }
+    }
+
+    public void setEmitterPosition(Vector3 pos)
+    {
+        Vector3 offset = pos - transform.position;
+        var shape = m_Rain.shape;
+        shape.position = offset;
+    }
+}
---
+++ LSystem/Assets/Weather/Rain/Snow.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snow : MonoBehaviour
+{
+    [Range(0.0f, 1.0f)]
+    public float m_Intensity = 0.0f;
+    private float m_IntensityCheck;
+    ParticleSystem m_SnowCache;
+    public int m_MaxParticles = 2000;
+    private SphereCollider collider;
+
+    public List<Sprite> snowflakeSprites;
+
+    public void SetIntensity(float a_SnowIntensity)
+    {
+        //m_SnowCache.maxParticles = (int)(m_MaxParticles * a_SnowIntensity) +m_MaxParticles * 5;
+        m_SnowCache.emissionRate = (int)(m_MaxParticles * a_SnowIntensity);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        m_IntensityCheck = m_Intensity;
+        m_SnowCache = GetComponent<ParticleSystem>();
+        //m_SnowCache.maxParticles = (int)(m_MaxParticles * m_Intensity) + m_MaxParticles * 5;
+        m_SnowCache.emissionRate = (int)(m_MaxParticles * m_Intensity);
+        collider = GetComponent<SphereCollider>();
+
+        foreach (Sprite s in snowflakeSprites)
+        {
+            m_SnowCache.textureSheetAnimation.AddSprite(s);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_Intensity != m_IntensityCheck)
+        {
+            SetIntensity(m_Intensity);
+            m_IntensityCheck = m_Intensity;
+        }
+    }
+
+    public void setEmitterPosition(Vector3 pos)
+    {
+        Vector3 offset = pos - transform.position;
+        var shape = m_SnowCache.shape;
+        shape.position = offset;
+        collider.center = offset;
+    }
+}
